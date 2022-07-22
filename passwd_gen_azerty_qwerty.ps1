7b4f20f4745753d2f2ab4c8d9802ce1ecbfade31
Romeo Permentier <56132002+ro-per@users.noreply.github.com>
2022-07-22 11:03:12 +0200

    Add paramerter input, increase change of symbols

--- passwd_gen_azerty_qwerty.ps1
+++ passwd_gen_azerty_qwerty.ps1
@@ -1,13 +1,17 @@
+param(
+    $Length=25,
+    $numbers="1234567890",
+    $letters="ertuiopsdfghjklxcvbnERTUIOPSDFGHJKLXCVBN",
+    $symbols="!"
+)
 
-
-$numbers="1234567890"
-$letters="ertuiopsdfghjklxcvbnERTUIOPSDFGHJKLXCVBN"
-$symbols="#!"
+# Increase chance of symbol
+$symbols= ($symbols*3)
 
 $characters="$numbers$letters$symbols".ToCharArray()
 
 $passwd=""
-foreach($i in 1..25) {
+foreach($i in 1..$Length) {
     $char = Get-Random -InputObject $characters -Count 1
     $passwd=$passwd+$char
 }
